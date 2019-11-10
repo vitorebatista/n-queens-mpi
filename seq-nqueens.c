979fc9e8f6cc73211143547f8d497442c8958817
Vitor Emanuel Batista <vitorebatista@gmail.com>
2019-11-10 16:19:09 -0300

    change seq

--- seq-nqueens.c
+++ seq-nqueens.c
@@ -16,26 +16,27 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+#include "time.c"
 
 #define FALSE 0
-#define TRUE  1
+#define TRUE 1
 
 long int Nunique = 0,
-         Ntotal  = 0;
+         Ntotal = 0;
 
 /* Maintenance procedure:  print a picture of the board. */
-void Picture (int Image[], int Size)
+void Picture(int Image[], int Size)
 {
    int Row, Col, Tst;
 
    for (Row = 0; Row < Size; Row++)
    {
-      putchar ('\n');
+      putchar('\n');
       Tst = Image[Row];
       for (Col = 0; Col < Size; Col++)
       {
-         putchar (' ');
-         putchar ( Col == Tst ? 'Q' : '.' );
+         putchar(' ');
+         putchar(Col == Tst ? 'Q' : '.');
       }
    }
    putchar('\n');
@@ -43,13 +44,20 @@ void Picture (int Image[], int Size)
 }
 
 /* Check two vectors for equality; return first inequality (a la strncmp) */
-int intncmp (int L[], int R[], int N)
+int intncmp(int L[], int R[], int N)
 {
    int Idx;
 
    for (Idx = 0; Idx < N; Idx++)
-      if ( L[Idx] - R[Idx] )
-         return L[Idx]-R[Idx];
+
+      if (L[Idx] - R[Idx])
+
+         return L[Idx] - R[Idx];
+      else
+      {
+         //printf("L: %d R: %d\n",L[Idx] , R[Idx]);
+      }
+
    return 0;
 }
 
@@ -58,12 +66,22 @@ void Rotate(int R[], int C[], int N, int Neg)
 {
    int Idx, Jdx;
 
-   Jdx = Neg ? 0 : N-1;
+   Jdx = Neg ? 0 : N - 1;
    for (Idx = 0; Idx < N; Neg ? Jdx++ : Jdx--)
       C[Idx++] = R[Jdx];
-   Jdx = Neg ? N-1 : 0;
+   Jdx = Neg ? N - 1 : 0;
    for (Idx = 0; Idx < N; Neg ? Jdx-- : Jdx++)
       R[C[Idx++]] = Jdx;
+}
+
+void printBoard(int Board[], int N)
+{
+   int Idx;
+   printf("\tBoard = ");
+   for (Idx = 0; Idx < N; Idx++)
+      printf("%d ", Board[Idx]);
+
+   printf("\n\n");
 }
 
 /* Vertical mirror:  reflect each row across the middle */
@@ -72,7 +90,7 @@ void Vmirror(int R[], int N)
    int Idx;
 
    for (Idx = 0; Idx < N; Idx++)
-      R[Idx] = (N-1) - R[Idx];
+      R[Idx] = (N - 1) - R[Idx];
    return;
 }
 
@@ -80,63 +98,85 @@ void Vmirror(int R[], int N)
 /* solution in the set of equivalent solutions; otherwise */
 /* return the number of equivalent solutions.             */
 int SymmetryOps(
-    int Board[],     /* The fully-populated board         */
-    int Trial[],     /* Used for symmetry checks          */
-                     /* Holds its own scratch space too!  */
-    int Size)        /* Number of cells in a row/column   */
-{  int  Idx;         /* Loop variable; intncmp result     */
-   int  Nequiv;      /* Number equivalent boards          */
-   int *Scratch=&Trial[Size];   /* Scratch space          */
+    int Board[], /* The fully-populated board         */
+    int Trial[], /* Used for symmetry checks          */
+                 /* Holds its own scratch space too!  */
+    int Size)    /* Number of cells in a row/column   */
+{
+   int Idx;                     /* Loop variable; intncmp result     */
+   int Nequiv;                  /* Number equivalent boards          */
+   int *Scratch = &Trial[Size]; /* Scratch space          */
 
-/* Copy; Trial will be subjected to the transformations   */
+   /* Copy; Trial will be subjected to the transformations   */
    for (Idx = 0; Idx < Size; Idx++)
       Trial[Idx] = Board[Idx];
 
-/* 90 degrees --- clockwise (4th parameter of Rotate is FALSE)*/
-   Rotate (Trial, Scratch, Size, 0);
-   Idx = intncmp (Board, Trial, Size);
-   if (Idx > 0) return 0;
-   if ( Idx == 0 )  /* No change on 90 degree rotation        */
+   /* 90 degrees --- clockwise (4th parameter of Rotate is FALSE)*/
+
+   Rotate(Trial, Scratch, Size, 0);
+   Idx = intncmp(Board, Trial, Size);
+   if (Idx > 0)
+      return 0;
+   if (Idx == 0)
+   { /* No change on 90 degree rotation        */
       Nequiv = 1;
-   else                                       /*  180 degrees */
-   {  Rotate (Trial, Scratch, Size, 0);
-      Idx = intncmp (Board, Trial, Size);
-      if (Idx > 0) return 0;
-      if ( Idx == 0 ) /* No change on 180 degree rotation     */
+      printf("nequiv = 1 \n ");
+      printBoard(Board, Size);
+      printBoard(Scratch, Size);
+   }
+   else /*  180 degrees */
+   {
+      Rotate(Trial, Scratch, Size, 0);
+      Idx = intncmp(Board, Trial, Size);
+      if (Idx > 0)
+         return 0;
+      if (Idx == 0)
+      { /* No change on 180 degree rotation     */
          Nequiv = 2;
-      else                                    /* 270 degrees  */
-      {  Rotate (Trial, Scratch, Size, 0);
-         Idx = intncmp (Board, Trial, Size);
-         if (Idx > 0) return 0;
+         printf("nequiv = 2 - ");
+         printBoard(Board, Size);
+      }
+      else /* 270 degrees  */
+      {
+         Rotate(Trial, Scratch, Size, 0);
+         Idx = intncmp(Board, Trial, Size);
+         if (Idx > 0)
+            return 0;
+         printf("nequiv = 4 - ");
+         printBoard(Board, Size);
          Nequiv = 4;
       }
    }
-/* Copy the board into Trial for rotational checks */
+   /* Copy the board into Trial for rotational checks */
    for (Idx = 0; Idx < Size; Idx++)
       Trial[Idx] = Board[Idx];
-/* Reflect -- vertical mirror */
-   Vmirror (Trial, Size);
-   Idx = intncmp (Board, Trial, Size);
-   if (Idx > 0) return 0;
-   if ( Nequiv > 1 )    // I.e., no four-fold rotational symmetry
+   /* Reflect -- vertical mirror */
+   Vmirror(Trial, Size);
+   Idx = intncmp(Board, Trial, Size);
+   if (Idx > 0)
+      return 0;
+   if (Nequiv > 1) // I.e., no four-fold rotational symmetry
    {
-/* -90 degrees --- equiv. to diagonal mirror */
-      Rotate (Trial, Scratch, Size, -1);
-      Idx = intncmp (Board, Trial, Size);
-      if (Idx > 0) return 0;
-      if ( Nequiv > 2 ) // I.e., no two-fold rotational symmetry
+      /* -90 degrees --- equiv. to diagonal mirror */
+      Rotate(Trial, Scratch, Size, -1);
+      Idx = intncmp(Board, Trial, Size);
+      if (Idx > 0)
+         return 0;
+      if (Nequiv > 2) // I.e., no two-fold rotational symmetry
       {
-/* -180 degrees --- equiv. to horizontal mirror */
-         Rotate (Trial, Scratch, Size, -1);
-         Idx = intncmp (Board, Trial, Size);
-         if (Idx > 0) return 0;
-/* -270 degrees --- equiv. to anti-diagonal mirror */
-         Rotate (Trial, Scratch, Size, -1);
-         Idx = intncmp (Board, Trial, Size);
-         if (Idx > 0) return 0;
+         /* -180 degrees --- equiv. to horizontal mirror */
+         Rotate(Trial, Scratch, Size, -1);
+         Idx = intncmp(Board, Trial, Size);
+         if (Idx > 0)
+            return 0;
+         /* -270 degrees --- equiv. to anti-diagonal mirror */
+         Rotate(Trial, Scratch, Size, -1);
+         Idx = intncmp(Board, Trial, Size);
+         if (Idx > 0)
+            return 0;
       }
    }
-/* WE HAVE A GOOD ONE! */
+   /* WE HAVE A GOOD ONE! */
    return Nequiv * 2;
 }
 
@@ -157,8 +197,11 @@ inline void Mark (
    AntiD[Idx] = Flag;
 }
  --- for straight C, make a #define macro   */
-#define Mark(R,C,Size,Diag,AntiD,Flag) \
-{  Diag[R-C+Size-1]=Flag; AntiD[R+C]=Flag;  }
+#define Mark(R, C, Size, Diag, AntiD, Flag) \
+   {                                        \
+      Diag[R - C + Size - 1] = Flag;        \
+      AntiD[R + C] = Flag;                  \
+   }
 
 /* Test the validity of this particular partial board.            */
 /* Massively used; inline to eliminate the function call overhead */
@@ -179,64 +222,65 @@ inline int Valid (int Board[], int Size, int Row,
 }
  --- for straight C, make a #define macro   */
 #define Valid(Board, Size, Row, Diag, AntiD) \
-!( Diag[Row-Board[Row]+Size-1] || AntiD[Row+Board[Row]] )
+   !(Diag[Row - Board[Row] + Size - 1] || AntiD[Row + Board[Row]])
 
 /* Process the partial (or complete) board for the indicated Row */
-void Nqueens (int Board[], int Trial[], int Size, int Row)
+void Nqueens(int Board[], int Trial[], int Size, int Row)
 {
    int Idx, Lim, Vtemp;
    static int *Diag, *AntiD = NULL;
 
-/* If the first call, allocate the boolean arrays Diag and AntiD */
-   if ( AntiD == NULL )
+   /* If the first call, allocate the boolean arrays Diag and AntiD */
+   if (AntiD == NULL)
    {
-/*    NOTE:  calloc initializes to all zero == FALSE  */
-      Diag  = (int *) calloc ( 2*Size-1, sizeof *Diag );
-      AntiD = (int *) calloc ( 2*Size-1, sizeof *AntiD );
-      if ( !AntiD )
+      /*    NOTE:  calloc initializes to all zero == FALSE  */
+      Diag = (int *)calloc(2 * Size - 1, sizeof *Diag);
+      AntiD = (int *)calloc(2 * Size - 1, sizeof *AntiD);
+      if (!AntiD)
       {
-         puts ("calloc failed --- out of memory!");
-         exit (6);
+         puts("calloc failed --- out of memory!");
+         exit(6);
       }
    }
-/* Check for a partial board. */
-   if (Row < Size-1)
+   /* Check for a partial board. */
+   if (Row < Size - 1)
    {
-      if (Valid (Board, Size, Row, Diag, AntiD))
+      if (Valid(Board, Size, Row, Diag, AntiD))
       {
-         Mark (Row, Board[Row], Size, Diag, AntiD, TRUE);
-         Nqueens (Board, Trial, Size, Row+1);
-         Mark (Row, Board[Row], Size, Diag, AntiD, FALSE);
+         Mark(Row, Board[Row], Size, Diag, AntiD, TRUE);
+         Nqueens(Board, Trial, Size, Row + 1);
+         Mark(Row, Board[Row], Size, Diag, AntiD, FALSE);
       }
-/*    Rejection of vertical mirror images means that row zero */
-/*    only needs to be processed to the middle.               */
-      Lim = Row ? Size : (Size+1)/2 ;
-      for (Idx = Row+1; Idx < Lim; Idx++)
+      /*    Rejection of vertical mirror images means that row zero */
+      /*    only needs to be processed to the middle.               */
+      Lim = Row ? Size : (Size + 1) / 2;
+      for (Idx = Row + 1; Idx < Lim; Idx++)
       {
          Vtemp = Board[Idx];
          Board[Idx] = Board[Row];
          Board[Row] = Vtemp;
-         if (Valid (Board, Size, Row, Diag, AntiD))
+         if (Valid(Board, Size, Row, Diag, AntiD))
          {
-            Mark (Row, Board[Row], Size, Diag, AntiD, TRUE);
-            Nqueens (Board, Trial, Size, Row+1);
-            Mark (Row, Board[Row], Size, Diag, AntiD, FALSE);
+            Mark(Row, Board[Row], Size, Diag, AntiD, TRUE);
+            Nqueens(Board, Trial, Size, Row + 1);
+            Mark(Row, Board[Row], Size, Diag, AntiD, FALSE);
          }
       }
-/*    Regenerate original vector from Row to Size-1:  */
+      /*    Regenerate original vector from Row to Size-1:  */
       Vtemp = Board[Row];
-      for (Idx = Row+1; Idx < Size; Idx++)
-         Board[Idx-1] = Board[Idx];
-      Board[Idx-1] = Vtemp;
+      for (Idx = Row + 1; Idx < Size; Idx++)
+         Board[Idx - 1] = Board[Idx];
+      Board[Idx - 1] = Vtemp;
    }
-/* This is a complete board.  Do the symmetry checks */
+   /* This is a complete board.  Do the symmetry checks */
    else
    {
-      if ( !Valid (Board, Size, Row, Diag, AntiD) )
+      if (!Valid(Board, Size, Row, Diag, AntiD))
          return;
-      Idx = SymmetryOps (Board, Trial, Size);
+      Idx = SymmetryOps(Board, Trial, Size);
       if (Idx)
       {
+         printf("idx=%d\n", Idx);
          Nunique++;
          Ntotal += Idx;
       }
@@ -244,43 +288,46 @@ void Nqueens (int Board[], int Trial[], int Size, int Row)
    return;
 }
 
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-   int   *Board, *Trial, Idx, Size;
-   FILE  *fptr;
+   double start_time = wtime();
+   double end_time, total_time;
+   int *Board, *Trial, Idx, Size;
+   FILE *fptr;
    double Clock, CPUstart, ClockStart, Lapsed;
 
    if (argc < 2)
    {
-      fputs ("Size:  ", stdout);
-      scanf ("%d", &Size);
+      fputs("Size:  ", stdout);
+      scanf("%d", &Size);
    }
    else
    {
       Size = atoi(argv[1]);
    }
 
-   Board = (int *) calloc (Size, sizeof *Board);
-   Trial = (int *) calloc (Size*2, sizeof *Board);
-/* Initial permutation generated. */
+   Board = (int *)calloc(Size, sizeof *Board);
+   Trial = (int *)calloc(Size * 2, sizeof *Board);
+   /* Initial permutation generated. */
    for (Idx = 0; Idx < Size; Idx++)
       Board[Idx] = Idx;
 
-   //getTimes(&ClockStart, &CPUstart);   // Start times.
-   Nqueens (Board, Trial, Size, 0);
-   //getTimes(&Clock, &Lapsed);          // finish times
-   Lapsed = Lapsed - CPUstart;
-   Clock  = Clock  - ClockStart;
+   //printBoard(Board, Size);
+   Nqueens(Board, Trial, Size, 0);
 
-   printf ("%3d ==> %10ld  %10ld  %10.4f  %10.4f\n",
-           Size, Nunique, Ntotal, Lapsed, Clock);
+   printf("%3d ==> %10ld  %10ld \n",
+          Size, Nunique, Ntotal);
+   end_time = wtime();
+   total_time = end_time - start_time;
+   printf("\nTempo de execução:\t%.6f sec \n", total_time);
 
-   fptr = fopen("Nqueens.csv", "a");
-   fprintf (fptr, "%3d,%10ld,%10ld,%10.4f,%10.4f",
-            Size, Nunique, Ntotal, Lapsed, Clock);
+   fptr = fopen("time-seq-nqueens.csv", "a");
 
-   fprintf (fptr, ",%5d\n", 4);
-   fclose (fptr);
+   fprintf(fptr, "%3d,%10ld,%10ld,%10.4f\n",
+           Size, Nunique, Ntotal, total_time);
+
+   //fprintf (fptr, ",%5d\n", 4);
+   fclose(fptr);
 
    return 0;
 }
